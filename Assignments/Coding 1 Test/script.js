014a18b51b7d0bfba18e33c4f50d8b90d388e3e8
Sam Wells <32932600+sllewmas@users.noreply.github.com>
2024-01-18 12:12:18 -0500

    updated Week 1 in-class

--- Assignments/Coding 1 Test/script.js
+++
@@ -1,5 +0,0 @@
-//↓↓↓↓↓↓↓↓↓↓↓↓YOUR CODE goes below here↓↓↓↓↓↓↓↓↓↓↓↓
-
-//↑↑↑↑↑↑↑↑↑↑YOUR CODE goes above here↑↑↑↑↑↑↑↑↑↑
-//Do not edit below this comment!
-module.exports = song; //This allows for auto-testing on Git-Hub classroom.
