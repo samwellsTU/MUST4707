87e8e4dab30858bffb11d41b8de07f9ad77392f3
Anthony123171 <156854076+Anthony123171@users.noreply.github.com>
2024-01-30 16:47:51 -0500

    Update script.js

--- In-Class.Code/Week.3/02.UserInput.Intro/script.js
+++ In-Class.Code/Week.3/02.UserInput.Intro/script.js
@@ -1,1 +1,20 @@
 // Introduction to User Input and Alerts in JavaScript
+
+let myname;
+
+myname = prompt("What is your name");
+let mood = prompt(`Hi, ${myname}! How are you?`);
+
+if (mood == "good") {
+  alert("That's nice to hear");
+}
+
+if (mood == "bad") {
+  prompt("Oh no, how can I help?");
+}
+
+let rating = prompt("on a scale of 1-10?");
+
+rating = parsenInt(rating);
+
+console.log(typeof rating);
