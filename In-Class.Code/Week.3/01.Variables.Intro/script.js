78231ffe471c3b0d32685c919491993977e868d0
Anthony123171 <156854076+Anthony123171@users.noreply.github.com>
2024-01-30 16:47:47 -0500

    Update script.js

--- In-Class.Code/Week.3/01.Variables.Intro/script.js
+++ In-Class.Code/Week.3/01.Variables.Intro/script.js
@@ -1,2 +1,29 @@
 // Introduction to Variables
 // Variables are like containers for storing data values.
+// let howYummy = "Yum!";
+// const yum = function () {
+//   var howYummy = "Yucky!";
+//   console.log(howYummy);
+// };
+
+// let bread = {
+//   topping: "butter",
+// };
+
+// console.log(bread.topping);
+
+// bread.topping = "PB&J";
+
+// console.log(bread.topping);
+
+// console.log(howYummy);
+// yum();
+
+if (4 > 2) {
+  var weather = "cloudy";
+  console.log("hey it's true");
+  if (34 > -5) {
+    console.log(weather);
+  }
+}
+console.log(weather);
