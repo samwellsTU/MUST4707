d33b2e7b5baae37bb923c6880ab274ae47cfe708
Anthony123171 <156854076+Anthony123171@users.noreply.github.com>
2024-01-30 16:47:53 -0500

    Update script.js

--- In-Class.Code/Week.3/07.Arrays.Summary/script.js
+++ In-Class.Code/Week.3/07.Arrays.Summary/script.js
@@ -1,10 +1,43 @@
 // Introduction to Arrays in JavaScript
 // Creating an Array
 
-let bread = "butter";
+let bread = {
+  topping: "butter",
+};
 
-console.log(bread);
+console.log(bread.topping);
 
-bread = "PB&J";
+bread.topping = "PB&J";
 
-console.log(bread);
+console.log(bread.topping);
+
+let fruits = ["apple", "banana", "cherry"];
+console.log("Fruits Array:", fruits);
+
+// Accessing Array Elements
+// Accessing the first element (index 0)
+console.log("First Fruit:", fruits[0]); // Output: apple
+
+// Accessing the third element (index 2)
+console.log("Third Fruit:", fruits[2]); // Output: cherry
+
+// Modifying Array Elements
+// Changing the second element
+fruits[1] = "blueberry";
+console.log("Modified Fruits Array:", fruits);
+
+// Array Length
+// Getting the number of elements in the array
+console.log("Number of Fruits:", fruits.length);
+
+// Adding Elements to an Array using push()
+fruits.push("mango");
+console.log("Added Mango:", fruits);
+
+// Removing the last element from an Array using pop()
+fruits.pop();
+console.log("Removed Last Fruit:", fruits);
+
+// Conclusion
+// Arrays are powerful and flexible structures for storing ordered data.
+// They have various methods to manipulate the data they hold.
