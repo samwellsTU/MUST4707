7ac34be9c79aaf9ac794b27440cbd4bc1b4e5140
Anthony123171 <156854076+Anthony123171@users.noreply.github.com>
2024-02-06 15:32:05 -0500

    Update script.js

--- In-Class.Code/Week.3/04.Conditionals.Intro/script.js
+++ In-Class.Code/Week.3/04.Conditionals.Intro/script.js
@@ -1,1 +1,11 @@
 // Introduction to Conditionals
+
+let chord = [60, 64, 67];
+
+if (chord[1] == 64) {
+  console.log("Major!");
+} else if (chord[1] == 64) {
+  console.log("Minor");
+} else if (chord[1] == 65) {
+  console.log("sus4");
+}
