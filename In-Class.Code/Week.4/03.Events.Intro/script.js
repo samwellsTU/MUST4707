46ecebf302aded59db55ae63dceb7e5e3b1a6bd6
Anthony123171 <156854076+Anthony123171@users.noreply.github.com>
2024-02-06 16:48:13 -0500

    Update script.js

--- In-Class.Code/Week.4/03.Events.Intro/script.js
+++ In-Class.Code/Week.4/03.Events.Intro/script.js
@@ -1,1 +1,14 @@
 //Event Interaction
+
+const button_click = function () {
+  let usr_input = document.getElementById("userInput");
+  console.log(usr_input.value);
+};
+
+const do_stuff = function () {
+  let button = document.getElementById("displayBtn");
+  button.addEventListener("click", button_click);
+  // console.log("stuff");
+};
+
+document.addEventListener("DOMContentLoaded", do_stuff);
