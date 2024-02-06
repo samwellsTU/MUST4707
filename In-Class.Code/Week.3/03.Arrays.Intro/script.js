4562f63c48db2ebe9e51316c7d2e94646943b5e9
Anthony123171 <156854076+Anthony123171@users.noreply.github.com>
2024-02-06 15:32:03 -0500

    Update script.js

--- In-Class.Code/Week.3/03.Arrays.Intro/script.js
+++ In-Class.Code/Week.3/03.Arrays.Intro/script.js
@@ -1,1 +1,35 @@
 // Introduction to Arrays
+
+let stuff = 23;
+
+if (true) {
+  const thing = "things";
+}
+
+const middleC = 60;
+
+let waveFile = [0, 0.01, 0.02, -0.24];
+
+let songInfo = ["myFile.wav", 2, true, stuff, middleC, [2, 3, 4]];
+console.log(waveFile);
+console.log(songInfo);
+
+songInfo.push(23);
+
+let myVar = songInfo.pop();
+
+songInfo.push(true);
+songInfo.pop();
+
+songInfo.shift();
+songInfo.unshift(12);
+
+console.log(songInfo.length);
+
+let chord = [60, 64, 67, 71];
+
+chord.push(74);
+
+chord.forEach((e) => {
+  console.log(e * 0.5);
+});
