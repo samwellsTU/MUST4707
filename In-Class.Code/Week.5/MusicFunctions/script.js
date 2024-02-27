b03391909e6b167d60ba839f9a74fd3b5988ef67
Anthony123171 <156854076+Anthony123171@users.noreply.github.com>
2024-02-27 15:36:33 -0500

    Update script.js

--- In-Class.Code/Week.5/MusicFunctions/script.js
+++ In-Class.Code/Week.5/MusicFunctions/script.js
@@ -1,18 +1,8 @@
+import MusicTools from "./MusicTools.js";
 
-/**
- * Converts a MIDI pitch number to a frequency in Hz.
- * @param {number} midiPitch - The MIDI pitch number.
- * @returns {number} The frequency in Hz.
- */
-const midiPitchToFrequency = function(midiPitch) {
-  return 440 * Math.pow(2, (midiPitch - 69) / 12);
-};
+letsomeAmp = MusicTools.dbtoa(-23);
+console.log(someAmp);
 
-/**
- * Converts a frequency in Hz to the corresponding MIDI pitch number.
- * @param {number} frequency - The frequency in Hz.
- * @returns {number} The MIDI pitch number.
- */
-const frequencyToMidiPitch = function(frequency) {
-  return 69 + 12 * Math.log2(frequency / 440);
-}
+MusicTools.frequencyToMidiPitch();
+
+console.log(MusicTools.midiPitchToFrequency(60));
