7aa4b31c838230a5165c89225f88672faa220495
Anthony123171 <156854076+Anthony123171@users.noreply.github.com>
2024-03-14 16:15:17 -0400

    Update script.js

--- In-Class.Code/Week.6/WebMidi.IO/script.js
+++ In-Class.Code/Week.6/WebMidi.IO/script.js
@@ -1,45 +1,34 @@
-// Enable WebMidi API and handle any errors if it fails to enable.
-// This is necessary to work with MIDI devices in the web browser.
 await WebMidi.enable();
 
-// Initialize variables to store the first MIDI input and output devices detected.
-// These devices can be used to send or receive MIDI messages.
 let myInput = WebMidi.inputs[0];
 let myOutput = WebMidi.outputs[0].channels[1];
 
-// Get the dropdown elements from the HTML document by their IDs.
-// These dropdowns will be used to display the MIDI input and output devices available.
 let dropIns = document.getElementById("dropdown-ins");
 let dropOuts = document.getElementById("dropdown-outs");
+let slider = document.getElementById("transpoSlider");
 
-// For each MIDI input device detected, add an option to the input devices dropdown.
-// This loop iterates over all detected input devices, adding them to the dropdown.
+slider.addEventListener("change", function () {
+  document.getElementById(
+    "transpoDisplay"
+  ).innerText = `${slider.value} semitones`;
+});
+
 WebMidi.inputs.forEach(function (input, num) {
   dropIns.innerHTML += `<option value=${num}>${input.name}</option>`;
 });
 
-// Similarly, for each MIDI output device detected, add an option to the output devices dropdown.
-// This loop iterates over all detected output devices, adding them to the dropdown.
 WebMidi.outputs.forEach(function (output, num) {
   dropOuts.innerHTML += `<option value=${num}>${output.name}</option>`;
 });
 
-// MIDI Processing Function
 const midiProcess = function (midiIN, transpose) {
-  let pitch = midiIN.not.number;
-
+  let pitch = midiIN.note.number;
   pitch += transpose;
-
   let myNewNote = new Note(pitch, { rawAttack: 101 });
-
   return myNewNote;
 };
 
-// Add an event listener for the 'change' event on the input devices dropdown.
-// This allows the script to react when the user selects a different MIDI input device.
 dropIns.addEventListener("change", function () {
-  // Before changing the input device, remove any existing event listeners
-  // to prevent them from being called after the device has been changed.
   if (myInput.hasListener("noteon")) {
     myInput.removeListener("noteon");
   }
@@ -47,34 +36,21 @@ dropIns.addEventListener("change", function () {
     myInput.removeListener("noteoff");
   }
 
-  // Change the input device based on the user's selection in the dropdown.
   myInput = WebMidi.inputs[dropIns.value];
 
-  // After changing the input device, add new listeners for 'noteon' and 'noteoff' events.
-  // These listeners will handle MIDI note on (key press) and note off (key release) messages.
   myInput.addListener("noteon", function (someMIDI) {
-    // When a note on event is received, send a note on message to the output device.
-    // This can trigger a sound or action on the MIDI output device.
-
     console.log(
-      `My not is ${someMIDI.note.identifier}, it is pitch ${someMIDI.note.number}, with a velocity of ${someMIDI.note.rawAttack}`
+      `My note is ${someMIDI.note.identifier}, it is pitch ${someMIDI.note.number}, with a velocity of ${someMIDI.note.rawAttack}`
     );
-
-    myOutput.sendNoteOn(midiProcess(someMIDI, 2));
+    myOutput.sendNoteOn(midiProcess(someMIDI, parseInt(slider.value)));
   });
 
   myInput.addListener("noteoff", function (someMIDI) {
-    // Similarly, when a note off event is received, send a note off message to the output device.
-    // This signals the end of a note being played.
     myOutput.sendNoteOff(someMIDI.note);
+    myOutput.sendNoteOn(midiProcess(someMIDI, parseInt(slider.value)));
   });
 });
 
-// Add an event listener for the 'change' event on the output devices dropdown.
-// This allows the script to react when the user selects a different MIDI output device.
 dropOuts.addEventListener("change", function () {
-  // Change the output device based on the user's selection in the dropdown.
-  // The '.channels[1]' specifies that the script should use the first channel of the selected output device.
-  // MIDI channels are often used to separate messages for different instruments or sounds.
   myOutput = WebMidi.outputs[dropOuts.value].channels[1];
 });
