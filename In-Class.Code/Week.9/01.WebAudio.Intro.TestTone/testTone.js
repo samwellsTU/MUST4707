9b3cdf8c38575c1974db138068f687a06d1e08ce
Anthony123171 <156854076+Anthony123171@users.noreply.github.com>
2024-03-21 16:06:19 -0400

    Create testTone.js

---
+++ In-Class.Code/Week.9/01.WebAudio.Intro.TestTone/testTone.js
@@ -0,0 +1,22 @@
+const myAudio = new AudioContext();
+let oscillator = null;
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+
+const startTone = function() {
+    myAudio.resume()
+    oscillator = myAudio.createOscillator()
+    oscillator.connect(myAudio.destination)
+    oscillator.type = "sawtooth"
+    oscillator.frequency.setValueAtTime(110, myAudio.currentTime)
+    oscillator.start()
+}
+
+const stopTone = function() {
+    oscillator.stop()
+}
+
+startButton.addEventListener("click", startTone);
+stopButton.addEventListener("click", stopTone)
+
+
